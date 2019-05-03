ed49feec7c52999650f117823e7b93be66f6bd1a
jacobgarrison4 <jacobdgarrison4@gmail.com>
2019-05-02 20:13:13 -0700

    Player movement added.

---
+++ Fluff the Penguin - Player Movement/Assets/Scripts/Movement.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float m_Speed = 100f;                 // How fast the player moves forward and back.
+    public float m_TurnSpeed = 220f;            // How fast the player turns in degrees per second.
+    public float jumpForce = 12f;
+
+    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
+    private string m_TurnAxisName;              // The name of the input axis for turning.
+    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
+    private float m_MovementInputValue;         // The current value of the movement input.
+    private float m_TurnInputValue;             // The current value of the turn input.
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnEnable()
+    {
+        // When the player moves, make sure it's not kinematic.
+        m_Rigidbody.isKinematic = false;
+
+        // Also reset the input values.
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+
+    private void OnDisable()
+    {
+        // When the player stops, set it to kinematic so it stops moving.
+        m_Rigidbody.isKinematic = true;
+    }
+
+
+    private void Start()
+    {
+        // The axes names are based on player number.
+        m_MovementAxisName = "Vertical";
+        m_TurnAxisName = "Horizontal";
+    }
+
+
+    private void Update()
+    {
+        // Store the value of both input axes.
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+    }
+
+    private void FixedUpdate()
+    {
+        // Adjust the rigidbodies position and orientation in FixedUpdate.
+        Move();
+        Turn();
+        Jump();
+    }
+
+
+    private void Move()
+    {
+        // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        // Apply this movement to the rigidbody's position.
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
+
+
+    private void Turn()
+    {
+        // Determine the number of degrees to be turned based on the input, speed and time between frames.
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        // Make this into a rotation in the y axis.
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        // Apply this rotation to the rigidbody's rotation.
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+    }
+
+    private void Jump()
+    {
+        Vector3 movement = new Vector3(m_MovementInputValue, 0.0f, m_TurnInputValue);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_Rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
